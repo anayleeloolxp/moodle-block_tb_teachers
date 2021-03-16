46779161ac345e2de37307d386e5d9a6843763ee
Anay <anay.infowind@gmail.com>
2021-03-16 17:49:50 +0530

    Removed data if wrong license

--- locallib.php
+++ locallib.php
@@ -52,6 +52,7 @@ function updateconfteachers() {
     if ($infoleeloolxp->status != 'false') {
         $leeloolxpurl = $infoleeloolxp->data->install_url;
     } else {
+        set_config('settingsjson', base64_encode($output), 'block_tb_teachers');
         return;
     }
     $url = $leeloolxpurl . '/admin/Theme_setup/get_instructors_settings';
