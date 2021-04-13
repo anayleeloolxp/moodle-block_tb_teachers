bf57e438515018dd5fda984d2742836f70a859d8
Anay Infowind <anay.infowind@gmail.com>
2021-04-13 13:07:42 +0530

    Added dependency for content api

--- version.php
+++ version.php
@@ -30,3 +30,6 @@ $plugin->requires = 2018050800; // Requires this Moodle version.
 $plugin->component = 'block_tb_teachers'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
+$plugin->dependencies = array(
+    'local_leeloolxpcontentapi' => 2021031907,
+);
