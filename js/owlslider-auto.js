b230381ef8df3069976968e933dd6878b2e0db1b
Anay Infowind <anay.infowind@gmail.com>
2021-04-21 16:09:33 +0530

    Item numbers

--- js/owlslider-auto.js
+++ js/owlslider-auto.js
@@ -2,28 +2,65 @@ require(["jquery"],function($) {
     
     $(document).ready(function() {
 
-        $('.tb_teachers').owlCarousel({
+        $('.layout_boxed .tb_teachers').owlCarousel({
             loop: true,
             margin: 10,
             responsiveClass: true,
             autoplay: true,
+            dots: false,
             responsive: {
                 0: {
                     items: 1,
-                    nav: true,
-                    dots: false
+                    nav: true
                 },
                 600: {
                     items: 2,
-                    nav: true,
-                    dots: false
+                    nav: false
                 },
                 1000: {
                     items: 2,
                     nav: true,
-                    dots: false
+                    dots: false,
+                    margin: 20
+                },
+                1300: {
+                    items: 2,
+                    nav: true,
+                    dots: false,
+                    margin: 20
                 }
             }
         });
+
+        $('body:not(.layout_boxed) .tb_teachers').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            autoplay: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+                },
+                600: {
+                    items: 2,
+                    nav: false
+                },
+                1000: {
+                    items: 2,
+                    nav: true,
+                    dots: false,
+                    margin: 20
+                },
+                1300: {
+                    items: 3,
+                    nav: true,
+                    dots: false,
+                    margin: 20
+                }
+            }
+        });
+
     });    
 });
