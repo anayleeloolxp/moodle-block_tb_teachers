7c75045141051815c21e686de38cbac986c80c2c
Anay Infowind <anay.infowind@gmail.com>
2021-07-05 11:39:56 +0530

    Added thirdpartyxml

--- block_tb_teachers.php
+++ block_tb_teachers.php
@@ -84,7 +84,7 @@ class block_tb_teachers extends block_base {
         $this->title = $resposedata->data->block_title;
         $autoslide = @$resposedata->data->autoslide;
 
-        $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_teachers/js/jquery.min.js'));
+        $this->page->requires->jquery();
         $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_teachers/js/owl.carousel.js'));
         if ($autoslide == 1) {
             $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_teachers/js/owlslider-auto.js'));
